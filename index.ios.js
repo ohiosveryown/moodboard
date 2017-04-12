004b7c6eccc5c7259ada43f29763ca8b2e755766
ohiosveryown <matt@ohiosveryown.co>
2017-04-11 21:41:08 -0400

    Splash Component

--- index.ios.js
+++ index.ios.js
@@ -6,43 +6,18 @@ import {
   Text,
   View
 } from 'react-native';
+import Splash from './components/Splash'
 
 export default class moodboard extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Nativeee!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Splash />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
 
 AppRegistry.registerComponent('moodboard', () => moodboard);
