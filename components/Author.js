4ceaa2d969ebdcc15d295f7fe7b72baaf3e4ce66
ohiosveryown <matt@ohiosveryown.co>
2017-04-16 14:25:12 -0400

    Author Component

---
+++ components/Author.js
@@ -0,0 +1,65 @@
+
+import React, { Component } from 'react';
+import {
+  /*AppRegistry,*/
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+import base from './Base';
+
+export default class Author extends Component {
+  render() {
+    return (
+
+      <View>
+
+        <View style={styles.viewWrapper} >
+          <Image
+            style={{ height: 48, width: 48 }}
+            source={require('../Assets/Imgs/michelle.png')}
+          />
+
+        <View style={styles.textWrapper}>
+          <Text style={styles.author}>
+            Created by: Michelle
+          </Text>
+
+          <Text style={styles.date}>
+            on May 12, 2017
+          </Text>
+        </View>
+
+        </View>
+
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  titleHeading: {
+    fontSize: 32,
+    fontFamily: 'univers-light',
+    //fontFamily: 'univers-condensedbold',
+  },
+  viewWrapper: {
+    flexDirection: 'row',
+  },
+  textWrapper: {
+    justifyContent: 'center',
+    marginLeft: 8,
+  },
+  author: {
+    fontFamily: 'Karla-Regular',
+    fontSize: 20,
+  },
+  date: {
+    fontFamily: 'Karla-Regular',
+    fontSize: 16,
+    opacity: 0.6,
+  }
+});
