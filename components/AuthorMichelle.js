66cd1f2becd75730e3e61717d9e53a8968dc0ada
ohiosveryown <matt@ohiosveryown.co>
2017-04-16 13:17:21 -0400

    Author Component

---
+++ components/AuthorMichelle.js
@@ -0,0 +1,30 @@
+
+import React, { Component } from 'react';
+import {
+  /*AppRegistry,*/
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import base from './Base';
+
+export default class AuthorMichelle extends Component {
+  render() {
+    return (
+
+        <Text>
+          Michelle
+        </Text>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  titleHeading: {
+    fontSize: 32,
+    fontFamily: 'univers-light',
+    //fontFamily: 'univers-condensedbold',
+  },
+});
