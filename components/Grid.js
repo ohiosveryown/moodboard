6c63b004e262054e88e5579a1a9ec4b7f2b51b6b
ohiosveryown <matt@ohiosveryown.co>
2017-04-17 07:07:18 -0400

    Grid Component

--- components/Grid.js
+++ components/Grid.js
@@ -17,17 +17,30 @@ import Author from './Author';
 
 var Lightbox = require('react-native-lightbox');
 const { height, width} = Dimensions.get('window');
-const activeProps = { resizeMode: 'contain', flex: 1, width };
+const activeProps = { resizeMode: 'contain', flex: 1, alignItems: 'flex-start', width };
+const activee = { alignItems: 'center', justifyContent: 'center', width, overflow: 'hidden' };
 
 export default class Main extends Component {
   render() {
     return (
-      <Lightbox activeProps={activeProps} backgroundColor="rgba(255,255,255,0.9)" underlayColor="white">
-        <Image
-          style={{ height: 100, resizeMode: 'contain' }}
-          source={require('../Assets/Imgs/lamp.png')}
-        />
-      </Lightbox>
+      <View>
+
+        <View>
+
+          <View>
+
+            <Lightbox activeProps={activee} backgroundColor="rgba(255,255,255,0.95)" underlayColor="white">
+              <Image
+                style={{ width: 320, height: 216, overflow: 'hidden'  }}
+                source={require('../Assets/Imgs/lamp.png')}
+              />
+            </Lightbox>
+
+          </View>
+
+        </View>
+
+      </View>
     );
   }
 }
@@ -52,6 +65,31 @@ const styles = StyleSheet.create({
     style={{ height: 100, resizeMode: 'contain' }}
     source={require('../Assets/Imgs/lamp.png')}
   />
+</Lightbox>
+
+/////////
+
+<Lightbox activeProps={activeProps} backgroundColor="rgba(255,255,255,0.9)" underlayColor="white">
+  <Image
+    style={{ width: 156, height: 104, resizeMode: 'contain' }}
+    source={require('../Assets/Imgs/chairs.png')}
+  />
+</Lightbox>
+
+<Lightbox activeProps={activee} backgroundColor="rgba(255,255,255,0.9)" underlayColor="white">
+  <Image
+    style={{ width: 156, height: 104,  }}
+    source={require('../Assets/Imgs/desk.png')}
+  />
+</Lightbox>
+
+</View>
+
+<Lightbox activeProps={activeProps} backgroundColor="rgba(255,255,255,0.9)" underlayColor="white">
+<Image
+  style={{ width: 319, height: 216, resizeMode: 'contain' }}
+  source={require('../Assets/Imgs/lamp.png')}
+/>
 </Lightbox>
 
 */
